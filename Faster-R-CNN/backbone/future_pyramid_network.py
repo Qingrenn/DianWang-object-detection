b237ecfe06bfcd255dcda8b4361dd991520d5f56
Qingren <yaoqingrenrobin@gmail.com>
2021-06-05 22:11:38 +0800

    add backbone

---
+++ Faster-R-CNN/backbone/future_pyramid_network.py
@@ -0,0 +1,101 @@
+from collections import OrderedDict
+
+import torch.nn as nn
+import torch
+from torch import Tensor
+import torch.nn.functional as F
+
+from torch.jit.annotations import Tuple, List, Dict
+"""
+in_channel_list: list[int],送入FPN的特征图的通道数,[256, 512, 1024, 2048]
+"""
+class FeaturePyramidNetwork(nn.Module):
+    def __init__(self, in_channel_list, out_channels, extra_blocks=None):
+        super(FeaturePyramidNetwork, self).__init__()
+        self.inner_blocks = nn.ModuleList()
+        self.layer_blocks = nn.ModuleList()
+
+        for in_channels in in_channel_list:
+            if in_channels == 0:
+                continue
+            inner_block_module = nn.Conv2d(in_channels, out_channels, 1)
+            layer_block_module = nn.Conv2d(out_channels, out_channels, 3, padding=1)
+            self.inner_blocks.append(inner_block_module)
+            self.layer_blocks.append(layer_block_module)
+
+        for m in self.children():
+            if isinstance(m , nn.Conv2d):
+                nn.init.kaiming_uniform_(m.weight, a=1) # 初始化卷积层参数
+                nn.init.constant_(m.bias, 0) # 偏置设置为0
+
+        self.extra_blocks = extra_blocks
+
+    def get_result_from_inner_blocks(self, x, idx):
+        # type: (Tensor, int) -> Tensor
+        """
+        self.inner_blocks[idx](x)
+        """
+        num_blocks = len(self.inner_blocks)
+        if idx < 0:
+            idx += num_blocks
+        out = x
+        for i, module in enumerate(self.inner_blocks):
+            if i == idx:
+                out = module(x)
+                break
+        return out
+    
+    def get_result_from_layer_blocks(self, x, idx):
+        # type: (Tensor, int) -> Tensor
+        """
+        self.layer_blocks[idx](x)
+        """
+        num_blocks = len(self.layer_blocks)
+        if idx < 0:
+            idx += num_blocks
+        out = x
+        for i, module in enumerate(self.layer_blocks):
+            if i == idx:
+                out = module(x)
+                break
+        return out
+
+    def forward(self, x):
+        # type: (Dict[str, Tensor]) -> Dict[str, Tensor]
+        """
+        x: OrderedDicr[layer_name: feature_map]
+        """
+        names = list(x.keys())
+        x = list(x.values())
+
+        results = [] # order: highest -> lowest resolution 
+
+        # first append the lowest resolution feature
+        last_inner = self.get_result_from_inner_blocks(x[-1], -1)
+        results.append(self.get_result_from_layer_blocks(last_inner))
+
+        # upsample
+        for idx in range(len(x)-2, -1, -1):
+            inner_lateral = self.get_result_from_inner_blocks(x[idx], idx)
+            feat_shape = inner_lateral.shape[-2:]
+            inner_top_down = F.interpolate(last_inner, size=feat_shape, mode="nearest")
+            last_inner = inner_lateral + inner_top_down
+            results.insert(0, self.get_result_from_layer_blocks(last_inner, idx))
+        
+        # 在layer4对应的预测特征层基础上生成预测特征矩阵5
+        if self.extra_blocks is not None:
+            results, names = self.extra_blocks(results, x, names)
+        
+        out = OrderedDict([(k, v) for k, v in zip(names, results)])
+        
+        return out
+
+class LastLevelMaxPool(torch.nn.Module):
+    """
+    Applies a max_pool2d on top of the last feature map
+    """
+    def forward(self, x, y, names):
+        # type: (List[Tensor], List[Tensor], List[str]) -> Tuple[List[Tensor], List[str]]
+        names.append("pool")
+        x.append(F.max_pool2d(x[-1], 1, 2, 0))  # input, kernel_size, stride, padding
+        return x, names
---
+++ Faster-R-CNN/backbone/resnet50_fpn_model.py
@@ -0,0 +1,211 @@
+import os
+from collections import OrderedDict
+
+import torch
+import torch.nn as nn
+from torch.jit.annotations import List, Dict
+from torch.nn.modules.linear import Identity
+from torchvision.ops.misc import FrozenBatchNorm2d
+
+from .future_pyramid_network import FeaturePyramidNetwork, LastLevelMaxPool
+
+class ResBlock(nn.Module):
+    expansion = 4
+    
+    def __init__(self, in_channel, out_channel, stride=1, downsample=None, norm_layer=None):
+        super(ResBlock, self).__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        
+        self.conv1 = nn.Conv2d(in_channels=in_channel, out_channels=out_channel,
+                                kernel_size=1, stride=1, bias=False)
+        self.bn1 = norm_layer(out_channel)
+
+        self.conv2 = nn.Conv2d(in_channels=out_channel, out_channels=out_channel,
+                                kernel_size=3, stride=stride, bias=False, padding=1)
+        self.bn2 = norm_layer(out_channel)
+        
+        self.conv3 = nn.Conv2d(in_channels=out_channel, out_channels=out_channel * self.expansion,
+                                kernel_size=3, stride=1, bias=False)
+        self.bn3 = norm_layer(out_channel * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        
+        self.downsample = downsample
+    
+    def forward(self, x):
+        identity = x
+        if self.downsample is not None:
+            identity = self.downsample(x)
+        
+        out = self.conv1(x)
+        out = self.bn1(x)
+        out = self.relu(x)
+
+        out = self.conv2(x)
+        out = self.bn2(x)
+        out = self.relu(x)
+        
+        out = self.conv3(x)
+        out = self.bn3(x)
+        
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, blocks_num, num_classes=1000, include_top=True, norm_layer=None):
+        super(ResNet, self).__init__()
+
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        self._norm_layer = norm_layer
+
+        self.include_top = include_top
+        self.in_channel = 64
+
+        self.conv1 = nn.Conv2d(3, self.in_channel, kernel_size=7, stride=2, padding=3, bias=1)
+        self.bn1 = norm_layer(self.in_channel)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, blocks_num[0])
+        self.layer2 = self._make_layer(block, 128, blocks_num[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, blocks_num[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, blocks_num[3], stride=2)
+        if self.include_top:
+            self.avgpool = nn.AdaptiveAvgPool2d((1,1))
+            self.fc = nn.Linear(512 * block.expansion, num_classes)
+        
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal(m.weight, model='fanout', nonlinearity='relu')
+
+    def _make_layer(self, block, channel, block_num, stride=1):
+        norm_layer = self._norm_layer
+        downsample = None
+        if stride != 1 or self.in_channel != channel * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channel, channel * block.expansion, kernel_size=1, stride=stride, bias=False),
+                norm_layer(channel * block.expansion)
+            )
+        
+        layers = []
+        layers.append(block(self.in_channel, channel, downsample=downsample,
+                            stride=stride, norm_layer=norm_layer))
+        self.in_channel = channel * block.expansion
+        
+        for _ in range(1, block_num):
+            layers.append(block(self.in_channel, channel, norm_layer=norm_layer))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        if self.include_top:
+            x = self.avgpool(x)
+            x = torch.flatten(x)
+            x = self.fc(x)
+
+        return x
+
+def overwrite_eps(model, eps):
+    """
+    This method overwrites the default eps values of all the
+    FrozenBatchNorm2d layers of the model with the provided value.
+    This is necessary to address the BC-breaking change introduced
+    by the bug-fix at pytorch/vision#2933. The overwrite is applied
+    only when the pretrained weights are loaded to maintain compatibility
+    with previous versions.
+    Args:
+        model (nn.Module): The model on which we perform the overwrite.
+        eps (float): The new value of eps.
+    """
+    for module in model.modules():
+        if isinstance(module, FrozenBatchNorm2d):
+            module.eps = eps
+
+
+class IntermediateLayerGetter(nn.ModuleDict):
+    """
+    Module wrapper that returns intermediate layers from a model
+    It has a strong assumption that the modules have been registered
+    into the model in the same order as they are used.
+    This means that one should **not** reuse the same nn.Module
+    twice in the forward if you want this to work.
+    Additionally, it is only able to query submodules that are directly
+    assigned to the model. So if `model` is passed, `model.feature1` can
+    be returned, but not `model.feature1.layer2`.
+    Arguments:
+        model (nn.Module): model on which we will extract the features
+        return_layers (Dict[name, new_name]): a dict containing the names
+            of the modules for which the activations will be returned as
+            the key of the dict, and the value of the dict is the name
+            of the returned activation (which the user can specify).
+    """
+    __annotations__ = {
+        "return_layers": Dict[str, str],
+    }
+
+    def __init__(self, model, return_layers):
+        if not set(return_layers).issubset([name for name, _ in model.named_children()]):
+            raise ValueError("return_layers are not present in model")
+
+        orig_return_layers = return_layers
+        return_layers = {str(k): str(v) for k, v in return_layers.items()}
+        layers = OrderedDict()
+
+        # 遍历模型子模块按顺序存入有序字典
+        # 只保存layer4及其之前的结构，舍去之后不用的结构
+        for name, module in model.named_children():
+            layers[name] = module
+            if name in return_layers:
+                del return_layers[name]
+            if not return_layers:
+                break
+
+        super(IntermediateLayerGetter, self).__init__(layers)
+        self.return_layers = orig_return_layers
+
+    def forward(self, x):
+        out = OrderedDict()
+        # 依次遍历模型的所有子模块，并进行正向传播，
+        # 收集layer1, layer2, layer3, layer4的输出
+        for name, module in self.items():
+            x = module(x)
+            if name in self.return_layers:
+                out_name = self.return_layers[name]
+                out[out_name] = x
+        return out
+
+class BackboneWithFPN(nn.Module):
+
+    def __init__(self, backbone, return_layers, in_channels_list, out_channels, extral_blocks=None):
+        super(BackboneWithFPN, self).__init__()
+
+        if extral_blocks is None:
+            extral_blocks = LastLevelMaxPool()
+
+        self.body = IntermediateLayerGetter(backbone, return_layers=return_layers)
+        self.fpn = FeaturePyramidNetwork(
+            in_channel_list=in_channels_list,
+            out_channels=out_channels,
+            extral_blocks=extral_blocks,
+        )
+
+        self.out_channels = out_channels
+
+    def forward(self, x):
+        x = self.body(x)
+        x = self.fpn(x)
+        return x
+
